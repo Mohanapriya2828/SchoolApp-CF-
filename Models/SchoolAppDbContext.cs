12f4a6684c7e7b21e8dc24c15b720990514cb5b8
Mohanapriya2828 <mohanapriya.jayabalan@psiog.com>
2025-10-19 18:06:05 +0530

    Data modeling and database implementation commit

---
+++ Models/SchoolAppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SchoolApp.Models
+{
+    public class SchoolAppDbContext : DbContext
+    {
+        public SchoolAppDbContext(DbContextOptions<SchoolAppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SchoolApp.Models
+{
+    public enum Role
+    {
+        Student,
+        Teacher
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DOB { get; set; }
+        public Role Designation { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string Department { get; set; }
+        public int? Age { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool IsActive { get; set; } = true;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+}
